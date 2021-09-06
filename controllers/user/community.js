d1ef626ae89033aa2ec579860eeaefb9365012ca
Sam <zaqaryan.sam@gmail.com>
2021-09-06 14:37:51 +0400

    send

--- controllers/user/community.js
+++ controllers/user/community.js
@@ -2,46 +2,36 @@ const mailer = require('../../sendMailer/nodemailer')
 const communityModel = require('../../models/community');
 const {communityValidate} = require('../../validation/communityValidate')
 
-exports.getCommunity = async (req, res, next) =>{
-    try {
-        return  res.render('community', {
-            staticData:req.staticData,
-            lang: req.session.language || 'en',
-        })
-    }catch (e) {
-        next(e)
-    }
-}
+exports.getCommunity = (req, res) => {
+    res.render('community', {
+        staticData:req.staticData,
+        lang: req.session.language || 'en',
+    })
+};
 
-
-exports.postAddCommunity = async (req,res,next) =>{
+exports.postAddCommunity = async (req, res, next) => {
     try {
-        const {error, value} = communityValidate(req.body);
-        // if (error) {
-        //     console.error('ValidationError', error.message);
-        //     return res.status(404).json(error)
-        // }
+        const {value} = communityValidate(req.body);
         const message = {
+
             to: req.body.email,
-            subject: 'ROOSH GROUP',
-            html:
-                `
-                <h3>congratulations! You are already a 'homecook'</h3>
-                <p>your details</p>
-                <ul>
-                    <li>Name: ${req.body.name} Surname: ${req.body.surname}</li>
-                    <li>Address: ${req.body.address} Phone: ${req.body.phone}</li>
-                    <li>Cooking level: ${req.body.subject}</li>
-                </ul>
-                `
+            subject: 'Congratulations! You are successfuly registred on our site',
+            text: `Congratulations! You are successfuly registred on our site
+       
+       your account details
+       
+                login: ${req.body.email}
+                password: ${req.body.pass}`
         }
         mailer(message)
         const cookContent = new communityModel({
             ...value
         })
-        await cookContent.save();
-        return res.redirect(`/${req.session.language || 'en'}`);
-    }catch (e) {
-        next(e)
+
+        await cookContent.save()
+        await setTimeout(function(){ res.redirect(`/${req.session.language || 'en'}`) }, 2000);
+
+    } catch (err) {
+        next(err)
     }
-}
+};
