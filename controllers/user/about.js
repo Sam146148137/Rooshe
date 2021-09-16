ef1d5c24a569540e3db3d2580c395a48dbdc4c0c
Sam <zaqaryan.sam@gmail.com>
2021-09-16 15:43:28 +0400

    read team members from db

--- controllers/user/about.js
+++ controllers/user/about.js
@@ -1,8 +1,24 @@
+const teamModel = require('../../models/team');
 
-exports.getAboutUS = (req, res) => {
-    res.render('about', {
-        staticData:req.staticData,
-        lang: req.session.language || 'en',
-        magia: req.originalUrl.substring(3),
-    })
+exports.getAboutUS = (req, res, next) => {
+    try{
+        const perPage = 6
+        const page = req.query.page || 1
+        teamModel.find({})
+            .exec(function(err, teamMember) {
+                teamModel.countDocuments().exec(function (err, count) {
+                    if (err) return console.error(`${err}`);
+                    res.render('about', {
+                        staticData:req.staticData,
+                        lang: req.session.language || 'en',
+                        teamMember,
+                        current: page,
+                        pages: Math.ceil(count / perPage),
+                        magia: req.originalUrl.substring(3),
+                    })
+                });
+            })
+    }catch (e) {
+        next(e)
+    }
 };
