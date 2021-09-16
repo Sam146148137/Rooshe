ad839b05fbc6a18252aa7bf2891d7c267e09d75e
Sam <zaqaryan.sam@gmail.com>
2021-09-16 09:04:10 +0400

    team creating

---
+++ controllers/admin/add-team.js
@@ -0,0 +1,52 @@
+// Including libs
+const multer = require('multer');
+const teamModel = require('../../models/team');
+
+// Create upload function that can be used in the router
+// Using Multer
+exports.upload = multer({
+    // Use storage to store the files
+    storage: multer.diskStorage ( {
+        destination: (req, file, cb) => {
+            cb(null, 'public/images');
+        },
+        filename: function (req, file, callback) {
+            callback(null, Date.now() + '--' + file.originalname)
+        }
+    })
+});
+
+// GetHome redirect to the home
+exports.getHome = (req, res) => {
+    res.render('admin/home')
+};
+
+exports.getAddTeam = (req, res) => {
+    res.render('admin/add_team');
+};
+
+exports.postAddTeam = (req, res) => {
+    console.log(req.file);
+    const x = new teamModel ();
+    x.name = req.body.name;
+    x.desc = req.body.desc;
+    x.image = req.file.filename;
+    x.save((err, doc) => {
+        if (!err) {
+            console.log('Team Member is saved successfully');
+            res.redirect('/admin/team');
+        }else {
+            console.log(err);
+        }
+    })
+};
+
+exports.getTeam = (req, res) => {
+    teamModel.find()
+        .then(function (doc) {
+            res.render('admin/team', {
+                item: doc
+            });
+        })
+};
+
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -6,15 +6,19 @@ const authMiddleware = require('../middleware/authMiddleware');
 
 const adminController = require('../controllers/admin/auth');
 const adminContentController = require('../controllers/admin/add-content-controller');
+const adminAddTeamController = require('../controllers/admin/add-team');
 const findController = require('../controllers/admin/findBlog');
 const editController = require('../controllers/admin/edit');
 const homecookController = require('../controllers/admin/homecook');
 
 const upload = adminContentController.upload;
+const uploadTeamMemberPhoto = adminAddTeamController.upload;
 
 // router.get('/signup', adminController.signupGet);
 
 // router.post('/signup', adminController.signupPost);
+
+router.get('/admin/home', adminAddTeamController.getHome);
 
 router.get('/admin', adminController.loginGet);
 
@@ -22,9 +26,15 @@ router.post('/admin', adminController.loginPost);
 
 router.get('/admin/add-content', authMiddleware.requireAuth, adminContentController.getAddContent);
 
+router.get('/admin/add_team', authMiddleware.requireAuth, adminAddTeamController.getAddTeam);
+
 router.post('/admin/add-content', authMiddleware.requireAuth, upload.single('image'), adminContentController.postAddContent);
 
+router.post('/admin/add_team', authMiddleware.requireAuth, uploadTeamMemberPhoto.single('image'), adminAddTeamController.postAddTeam);
+
 router.get('/admin/blogs', authMiddleware.requireAuth, adminContentController.getContent);
+
+router.get('/admin/team', authMiddleware.requireAuth, adminAddTeamController.getTeam);
 
 //homecooks get
 router.get('/admin/homecooks', authMiddleware.requireAuth, homecookController.gethomecook);
@@ -38,10 +48,8 @@ router.get('/admin/edit/:id', authMiddleware.requireAuth, editController.editBlo
 
 // Update post
 router.post('/admin/update/:id', authMiddleware.requireAuth, upload.single('image'), editController.updateBlog);
-
 //Delete Blog
 router.get('/admin/delete/:id', authMiddleware.requireAuth, editController.deleteBlog);
-
 
 
 module.exports = router;
